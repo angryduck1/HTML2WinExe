1bbb16002d87c70c55f11945f5fae274432d6814
Sivokon Sava <savakapro@gmail.com>
2024-10-16 19:04:13 +0300

    AddBackgroundView

--- Html/HtmlPageViewer.py
+++ Html/HtmlPageViewer.py
@@ -13,24 +13,24 @@ class HTML:
             with open("your_file.html", "r", encoding="utf-8") as file:
                 self.html_content = file.read()
         except FileNotFoundError:
-            messagebox.showerror("Ошибка", "Файл не найден")
+            messagebox.showerror("Error", "File is not Found")
         except Exception as e:
-            messagebox.showerror("Ошибка", str(e))
+            messagebox.showerror("Error", str(e))
 
     def get_title(self):
         soup = BeautifulSoup(self.html_content, "html.parser")
         title = soup.title.string
         return title
-
+    
     def display_html(self):
         self.read_html()
         modified_html = EditHtmlElements(self.html_content).delete_elements()
-        
+
         root = Tk()
         root.title(self.get_title())
         root.geometry("400x400")
 
-        html_tk = HTMLLabel(root, html=modified_html)
+        html_tk = HTMLLabel(root, html=modified_html, background=EditHtmlElements(self.html_content).get_background_color())
         html_tk.pack(padx=0, pady=0, expand=True, fill=BOTH)
 
         root.mainloop()
--- Utilites/EditHtmlElements.py
+++ Utilites/EditHtmlElements.py
@@ -8,3 +8,14 @@ class EditHtmlElements:
         soup = BeautifulSoup(self.html_content, "html.parser")
         soup.title.decompose()
         return str(soup)
+    
+    def get_background_color(self):
+        soup = BeautifulSoup(self.html_content, "html.parser")
+        body = soup.find('body')
+        if body and 'style' in body.attrs:
+            style = body['style']
+            if 'background-color' in style:
+                color_value = style.split("background-color:")[1].strip().rstrip(";")
+                return color_value
+        else:
+            print("Background color is not found")
